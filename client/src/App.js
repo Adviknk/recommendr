ab20cf01e4f2557975d2145aed0a426283ecd31b
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-07-19 15:58:39 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [backendData, setBackendData] = useState([{}])
   
   useEffect(() => {
-    fetch("/api").then(
+    fetch("/").then(
       response => response.json()
     ).then(
       data => {
