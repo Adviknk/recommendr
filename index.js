6db916faea413f1dc39c1addfd3fd05c2b6f2272
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-07-24 15:11:59 -0400

    commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(process.env.USER);
+const { readFile, readFileSync } = require('fs')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+
+    readFile('./navigation.html', 'utf8', (err, html) => {
+        
+        response.send(html);
+    });
+
+});
+
+
+app.listen(process.env.PORT || 3000, () => console.log('App is available on http://localhost:3000'));
