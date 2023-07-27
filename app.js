95d7d2a978aa38e3a924cfb938fea12b0d34abf7
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-07-27 10:58:30 -0400

    commit

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ app.get(['/', '/home'], (req, res) => {
     res.render('main', data);
 });
 
+app.get(['/account'], (req, res) => {
+  const data = {
+    pageTitle: 'Account | Recommendr',
+    file: 'account',
+    variables: {
+      var1: 'First Variable',
+      var2: 'Second Variable'
+    }
+  };
+  res.render('main', data);
+});
+
+app.get(['/post'], (req, res) => {
+  const data = {
+    pageTitle: 'Post | Recommendr',
+    file: 'post',
+    variables: {
+      var1: 'First Variable',
+      var2: 'Second Variable'
+    }
+  };
+  res.render('main', data);
+});
+
 
 // Where the app is running
 app.listen(process.env.PORT || 3000, () => console.log('App is available on http://localhost:3000'));
