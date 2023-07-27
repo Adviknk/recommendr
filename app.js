f14d107be6471b91316ca83be2a4e03a58440e79
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-07-27 11:24:48 -0400

    commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const session = require('express-session'); 
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get(['/', '/home'], (req, res) => {
     const data = {
@@ -13,31 +21,70 @@ app.get(['/', '/home'], (req, res) => {
         var2: 'Second Variable'
       }
     };
+    // Checking if the username is there
+    console.log(req.session.username)
     res.render('main', data);
+
 });
 
+app.get(['/login'], (req, res) => {
+  if (req.session.username) {
+    // if 'username' is already in session
+    res.redirect('/dashboard');
+  } else {
+    const data = {
+      pageTitle: 'Log In | Recommendr',
+      file: 'login',
+      variables: {
+
+      }
+    };
+    res.render('main', data);
+  }
+});
+
+app.post('/login', (req, res) => {
+  valid = true;
+  if(valid){
+    const { username } = req.body;
+    req.session.username = username;
+    res.redirect('/home');
+  }
+  else {
+    res.redirect('/login');
+  }
+});
+
+
 app.get(['/account'], (req, res) => {
-  const data = {
-    pageTitle: 'Account | Recommendr',
-    file: 'account',
-    variables: {
-      var1: 'First Variable',
-      var2: 'Second Variable'
-    }
-  };
-  res.render('main', data);
+  if (req.session.username) {
+    const data = {
+      pageTitle: 'Account | Recommendr',
+      file: 'account',
+      variables: {
+
+      }
+    };
+    res.render('main', data);
+    //res.send(`Welcome to your dashboard, ${req.session.username}!`);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get(['/post'], (req, res) => {
-  const data = {
-    pageTitle: 'Post | Recommendr',
-    file: 'post',
-    variables: {
-      var1: 'First Variable',
-      var2: 'Second Variable'
-    }
-  };
-  res.render('main', data);
+  if (req.session.username) {
+    const data = {
+      pageTitle: 'Post | Recommendr',
+      file: 'post',
+      variables: {
+
+      }
+    };
+    res.render('main', data);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 
