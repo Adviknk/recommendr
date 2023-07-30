9645a61f7f652536ae5817ed9a44c9d1bade7524
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-07-30 17:47:17 -0400

    commit

--- posts.js
+++ posts.js
@@ -15,23 +15,7 @@ post_create = (req, res) => {
   console.log(req.body)
   console.log(req.file)
   const keysArray = Object.keys(req.body);
-  // upload.single('imageUpload')(req, res, (err) => {
-  //   if (err) {
-  //     console.error('Error uploading file:', err);
-  //     return res.status(500).send('Error uploading the file.');
-  //   }
-    
-  //   // If the file upload is successful, the file details will be available in req.file
-  //   if (req.file) {
-  //     console.log('File uploaded successfully:', req.file.filename);
-  //     // You can save the file details to a database or use them as needed
-
-  //     // Redirect to another page or respond with a success message
-  //   } else {
-  //     // If no file was uploaded or an error occurred, handle accordingly
-  //     res.status(400).send('Error uploading the file.');
-  //   }
-  // });
+  
   if(keysArray.length != 5) {
     res.redirect('/post')
   }
