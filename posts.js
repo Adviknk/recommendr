da5e91f138f4de410c6ef230c0c86a452da6bce8
Advik22 <128092185+Adviknk@users.noreply.github.com>
2023-07-30 23:19:25 -0400

    commit

--- posts.js
+++ posts.js
@@ -1,4 +1,5 @@
 const { user_info } = require('./get-info');
+const { quick_run } = require('./sql-conn');
 const multer = require('multer');
 
 
@@ -16,10 +17,44 @@ post_create = (req, res) => {
   console.log(req.file)
   const keysArray = Object.keys(req.body);
   
-  if(keysArray.length != 5) {
+  count = 0;
+
+  if(keysArray.includes('Food')) {
+    count = count + 1;
+  }
+  if(keysArray.includes('Other')) {
+    count = count + 1;
+  }
+  if(keysArray.includes('Activity')) {
+    count = count + 1;
+  }
+  if(keysArray.includes('Entertainment')) {
+    count = count + 1;
+  }
+
+  stars = 0
+
+  if(keysArray.includes('one')) {
+    stars = 1;
+  }
+  if(keysArray.includes('two')) {
+    stars = 2;
+  }
+  if(keysArray.includes('three')) {
+    stars = 3;
+  }
+  if(keysArray.includes('four')) {
+    stars = 4;
+  }
+  if(keysArray.includes('five')) {
+    stars = 5;
+  }
+  
+  if(stars == 0 || count > 1 || count == 0) {
     res.redirect('/post')
   }
   else {
+    quick_run("INSERT INTO recommendations (title, description, image, url, address, category, stars) VALUES ('" + req.body.title + "','" + req.body.description + "','" + req.file.originalname + "','" + req.body.url + "','" + req.body.address + "','" + keysArray[2] + "','" + stars + "')")
     res.redirect('/home')
   }
 }
